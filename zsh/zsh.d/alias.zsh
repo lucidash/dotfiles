f9827e74d2b49b4a169612c93dac74949adf5fc7
SangHoon Kim <lucidash@LucidashMPR.local>
2014-08-08 16:52:10 +0900

    Add screen aliasing

---
+++ zsh/zsh.d/alias.zsh
@@ -0,0 +1,12 @@
+# own aliasing 
+alias bit='ssh lucidash@bit.sparcs.org'
+alias toy2='ssh user2@172.16.100.158'
+alias sw='ssh user2@61.43.139.132'
+alias jj='python manage.py'
+alias l='ls -G'
+
+
+
+# screen aliasing
+alias sls='screen -ls'
+alias snew='screen -S'
