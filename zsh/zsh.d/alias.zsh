8590d9804902c795d0abdb64b2dfd475256b682e
SangHoon Kim <lucidash@lucidashMBA.local>
2014-08-20 03:11:29 +0900

    Tmux alias added

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -1,11 +1,10 @@
 # own aliasing 
-# ssh aliasing 
+#
+# ssh ========================================= {{{
 alias bit='ssh lucidash@bit.sparcs.org'
 alias toy2='ssh user2@172.16.100.158'
 alias sw='ssh user2@61.43.139.132'
 alias sparcs='ssh lucidash@sparcs.org'
-
-
 
 alias tmdrb='ssh lucidash@14.63.218.219'
 alias tmdrb2='ssh lucidash@14.63.218.219 -p 23'
@@ -13,15 +12,35 @@ alias tmdrb3='ssh lucidash@14.63.218.219 -p 24'
 
 
 
-alias jj='python manage.py'
-alias l='ls -G'
+
+
+# Tmux ========================================= {{{
+#
+# # create a new session with name
+alias tmuxnew='tmux new -s'
+# # list sessions
+alias tmuxl='tmux list-sessions'
+# # tmuxa <session> : attach to <session> (force 256color and detach others)
+alias tmuxa='tmux -2 attach-session -d -t'
+#
+# # }}}
 
 
 
-# screen aliasing
+# screen ========================================= {{{
+# 
+# # list sessions
 alias sls='screen -ls'
+# # create a new session with name 
 alias snew='screen -S'
+# # sdr <session> : attach to <session> and detach others
 alias sdr='screen -d -r'
+#
+# # }}}
 
 #tail -f to tailf
 alias tailf='tail -f'
+
+#others
+alias jj='python manage.py'
+alias l='ls -G'
