0de155ecb0479d874773c94712dcdbe2a5c7bb06
SangHoon Kim <lucidash@lucidashMBA.local>
2014-08-19 13:10:23 +0900

    aliasing added

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -1,8 +1,18 @@
 # own aliasing 
+# ssh aliasing 
 alias bit='ssh lucidash@bit.sparcs.org'
 alias toy2='ssh user2@172.16.100.158'
 alias sw='ssh user2@61.43.139.132'
 alias sparcs='ssh lucidash@sparcs.org'
+
+
+
+alias tmdrb='ssh lucidash@14.63.218.219'
+alias tmdrb2='ssh lucidash@14.63.218.219 -p 23'
+alias tmdrb3='ssh lucidash@14.63.218.219 -p 24'
+
+
+
 alias jj='python manage.py'
 alias l='ls -G'
 
