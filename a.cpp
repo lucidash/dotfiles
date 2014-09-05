ecee9ca485f934adbb24baa194084b7433063a7d
lucidash <lucidash@lucidashui-MacBook-Pro.local>
2014-09-06 02:11:26 +0900

    aaa

---
+++ a.cpp
@@ -0,0 +1,23 @@
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main() {
+    cin.sync_with_stdio(false);
+    freopen("input.txt", "rt", stdin);
+    freopen("output.txt", "wt", stdout);
+    
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
